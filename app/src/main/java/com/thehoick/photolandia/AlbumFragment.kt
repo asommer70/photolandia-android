c5160e0483a49fe7a6b04420c753debd18f691f0
Adam Sommer <adam@thehoick.com>
2018-03-20 20:53:50 -0400

    Removed all Album details but the name to make things look better.

--- app/src/main/java/com/thehoick/photolandia/AlbumFragment.kt
+++ app/src/main/java/com/thehoick/photolandia/AlbumFragment.kt
@@ -1,32 +1,21 @@
 package com.thehoick.photolandia
 
-
 import android.app.Fragment
 import android.graphics.Color
 import android.os.Bundle
-import android.support.constraint.ConstraintLayout
 import android.support.design.widget.BottomNavigationView
 import android.support.design.widget.FloatingActionButton
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.*
+import android.view.View.INVISIBLE
 import android.view.ViewGroup
-import android.widget.AbsListView
 import android.widget.GridView
 import android.widget.ProgressBar
 import android.widget.TextView
-import kotlinx.android.synthetic.main.album_item.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import java.text.SimpleDateFormat
-import android.support.constraint.ConstraintSet
-import android.widget.LinearLayout
-
-
-
-
 
 class AlbumFragment: Fragment() {
     val TAG = AlbumFragment::class.java.simpleName
@@ -51,13 +40,7 @@ class AlbumFragment: Fragment() {
 
         val api = Api(view.context)
         val albumDetailName = view.findViewById<TextView>(R.id.albumDetailName)
-        val albumDetailDescription = view.findViewById<TextView>(R.id.albumDetailDescription)
-        val albumDetailId = view.findViewById<TextView>(R.id.albumDetailId)
-        val albumDetailCreatedAt = view.findViewById<TextView>(R.id.albumDetailCreatedAt)
-        val albumDetailUpdatedAt = view.findViewById<TextView>(R.id.albumDetailUpdatedAt)
         val progressAlbumDetails = view.findViewById<ProgressBar>(R.id.progressAlbumDetails)
-        val albumContainer = view?.findViewById<ConstraintLayout>(R.id.albumContainer)
-        val albumHeader = view?.findViewById<ConstraintLayout>(R.id.albumHeader)
 
         progressAlbumDetails.visibility = View.VISIBLE
 
@@ -73,64 +56,9 @@ class AlbumFragment: Fragment() {
                 val albumDetailPhotosView = view.findViewById<GridView>(R.id.albumDetailPhotos)
                 Log.d(TAG, "response?.body()?.name: ${response?.body()?.name}")
 
-                albumDetailPhotosView.setOnScrollListener(object: AbsListView.OnScrollListener {
-                    override fun onScroll(view: AbsListView?, firstVisibleItem: Int, visibleItemCount: Int, totalItemCount: Int) {
-                        Log.d(TAG, "onScroll visibleItemCount: $visibleItemCount")
-                        Log.d(TAG, "onScroll firstVisibleItem: $firstVisibleItem")
-                        if (firstVisibleItem != 0) {
-                            albumDetailDescription.visibility = GONE
-                            albumDetailId.visibility = GONE
-                            albumDetailCreatedAt.visibility = GONE
-                            albumDetailUpdatedAt.visibility = GONE
-
-                            albumDetailUpdatedAt.height = GONE
-                            albumHeader?.systemUiVisibility = GONE
-
-                            // Gets linearlayout
-                            val params = albumHeader?.getLayoutParams()
-                            params?.height = 0
-                            params?.width = 0
-                            albumHeader?.setLayoutParams(params)
-
-//                            albumDetailPhotosView.layoutParams.height = albumDetailPhotosView.layoutParams.height + 10
-//                            albumDetailPhotosView.height = 439
-
-//                            albumDetailPhotosView
-//                            albumContainer?.removeView(albumDetailCreatedAt)
-//                            albumContainer?.removeView(albumDetailUpdatedAt)
-//                            albumContainer?.removeView(albumDetailId)
-//                            albumContainer?.removeView(albumDetailDescription)
-//                            albumContainer?.removeView(albumHeader)
-                        } else {
-                            albumDetailDescription.visibility = VISIBLE
-                            albumDetailDescription.height = 34
-                            albumDetailId.visibility = VISIBLE
-                            albumDetailId.height = 25
-                            albumDetailCreatedAt.visibility = VISIBLE
-                            albumDetailCreatedAt.height = 18
-                            albumDetailUpdatedAt.visibility = VISIBLE
-                            albumDetailUpdatedAt.height = 18
-//                            359
-//                            albumContainer?.addView(albumDetailCreatedAt)
-//                            albumContainer?.addView(albumDetailUpdatedAt)
-//                            albumContainer?.addView(albumDetailId)
-//                            albumDetailPhotosView.layoutParams.height = albumDetailPhotosView.layoutParams.height + 10
-                        }
-//                        albumDetailName.visibility = INVISIBLE
-                    }
-
-                    override fun onScrollStateChanged(view: AbsListView?, scrollState: Int) {
-                    }
-                })
-
                 progressAlbumDetails.visibility = INVISIBLE
 
                 albumDetailName.setText(response?.body()?.name)
-                albumDetailDescription.setText(response?.body()?.description)
-                albumDetailId.setText("Album ID Number: ${response?.body()?.id.toString()}")
-                val df = SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
-                albumDetailCreatedAt.setText("Created: " + df.format(response?.body()?.created_at))
-                albumDetailUpdatedAt.setText("Updated: " + df.format(response?.body()?.updated_at))
 
                 albumPhotoAdapter = AlbumPhotoAdapter(activity, response?.body()?.id!!, response.body()?.photo_set!!)
                 albumDetailPhotosView.adapter = albumPhotoAdapter
